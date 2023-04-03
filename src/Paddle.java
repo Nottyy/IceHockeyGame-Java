1c2f155f3226be8a167246205e06ffd0fcfc3c81
Antonio <antonio.marinovvvv@gmail.com>
2023-04-03 16:39:12 +0300

    class Ball

--- src/Paddle.java
+++ src/Paddle.java
@@ -1,8 +1,31 @@
 package src;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 public class Paddle {
 
     public Paddle(){
+
+    }
+
+    public void keyPressed(KeyEvent e){
+
+    }
+
+    public void keyReleased(KeyEvent e){
+
+    }
+
+    public void setYDirection(int yDirection){
+
+    }
+
+    public void move(){
+
+    }
+
+    public void draw(Graphics g){
 
     }
 }
